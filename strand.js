8201822a519290701ab82b7c3df5a99c393d2b6e
patrick <patrick.chen31@gmail.com>
2022-10-20 15:01:00 +0700

    update strand sort

--- strand.js
+++ strand.js
@@ -1,27 +1,49 @@
 import chalk from "chalk";
 
-let arr = [10,5,30,40,2,4,9];
+let arr = [5, 1, 4, 2, 0, 9, 6, 3, 8, 7];
+let firstTime=true;
 
 function strand(arr,output){
   if(!arr.length) return;
-  let sublist = []
+  const sublist = []
+  console.log(chalk.blueBright(`original arr: ${arr}`))
   sublist.push(arr.shift());
-  console.log(chalk.blueBright(`arr: ${arr}`))
-  console.log(chalk.blueBright(`currVal: ${sublist[0]}`))
+  // console.log(chalk.blueBright(`currVal: ${sublist[0]}`))
 
-  for(let i=0; i<arr.length; i++){
-    print(arr,arr[i],sublist[0],"yellow")
-    if(arr[i]>sublist[0]){
+  for(let i=0,j=0; i<arr.length; i++){
+    print(arr,arr[i],sublist[j],"yellow")
+    process.stdout.write(` ${arr[i]}>${sublist[j]} ?`);
+
+    if(arr[i]>sublist[j]){
       process.stdout.write(" ==> ");
-      print(arr,arr[i],sublist[0],"green")
+      print(arr,arr[i],sublist[j],"green")
       sublist.push(arr[i]);
       arr.splice(i,1)
       i--;
+      j++;
     }
     console.log('')
   }
-  ;
-  output.unshift(...sublist)
+
+  console.log(chalk.blueBright(`sublist: ${sublist}`))
+
+  if(firstTime){
+    output.unshift(...sublist)
+    firstTime=false;
+  }else{
+    let subListEnd = sublist.length-1;
+    let outputStart = 0;
+    while(sublist.length>0){
+      if(sublist[subListEnd]>output[outputStart]){
+        outputStart++;
+      }else{
+        output.splice(outputStart,0,sublist[subListEnd])
+        sublist.splice(subListEnd,1);
+        subListEnd--;
+        outputStart=0;
+      }
+    }
+  }
   console.log(chalk.blueBright(`output: ${output}\n`))
 
   strand(arr,output);
@@ -46,3 +68,7 @@ function print(arr,a,b,col){
   }
   process.stdout.write(`]`);
 }
+
+/*
+https://en.wikipedia.org/wiki/Strand_sort
+*/
